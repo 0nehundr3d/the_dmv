7631c113c74db973b95b39fb7b719f270d8efb2b
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 13:45:44 -0600

    Add registered_vehicles attribute to facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,16 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+  :address,
+  :phone,
+  :services,
+  :registered_vehicles
 
   def initialize(info_hash)
     @name = info_hash[:name]
     @address = info_hash[:address]
     @phone = info_hash[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
