98ced940cbaee56f4de9fcdfdd5f09e2657853b5
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 16:11:40 -0600

    Add create vehicles method to vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,3 +1,16 @@
 class VehicleFactory
-    
+    def create_vehicles(dataset)
+        vehicles = []
+        dataset.each do |vehicle|
+            new_vehicle = Vehicle.new({
+                :vin=>vehicle[:vin_1_10],
+                :year=>vehicle[:model_year],
+                :make=>vehicle[:make],
+                :model=>vehicle[:model],
+                :engine=>vehicle.include?(:electric_range) ? :ev : :ice
+            })
+            vehicles << new_vehicle
+        end
+        vehicles
+    end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe VehicleFactory do
             expect(@factory).to be_a(VehicleFactory)
         end
     end
+
+    describe "create_vehicles" do
+        it "can create vehicles" do
+            wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+            vehicles = @factory.create_vehicles(wa_ev_registrations)
+
+            expect(vehicles[0]).to be_a(Vehicle)
+            expect(vehicles[0].engine).to eq(:ev)
+            expect(vehicles[0].make).to eq("BMW")
+            expect(vehicles[0].model).to eq("i3")
+            expect(vehicles[0].vin).to eq("WBY8P8C51K")
+            expect(vehicles[0].year).to eq("2019")
+        end
+    end
 end
