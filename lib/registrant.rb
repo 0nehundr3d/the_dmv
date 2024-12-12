143e38d116439d74dfb39c2eac9832b394cba5b6
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 13:30:41 -0600

    Add age attribute

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,7 +1,8 @@
 class Registrant 
-    attr_reader :name
+    attr_reader :name, :age
 
     def initialize(name, age, permit = false)
         @name = name
+        @age = age
     end
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -17,5 +17,10 @@ RSpec.describe Registrant do
             expect(@registrant_1.name).to eq("Bruce")
             expect(@registrant_2.name).to eq("Penny")
         end
+
+        it "has an age" do
+            expect(@registrant_1.age).to eq(18)
+            expect(@registrant_2.age).to eq(15)
+        end
     end
 end
