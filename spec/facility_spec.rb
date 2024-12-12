4adca0c55b3c5813f2b742870793e7fe611c06bb
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 14:40:21 -0600

    Remove redundant facility object

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -4,7 +4,6 @@ require 'date'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
 
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
