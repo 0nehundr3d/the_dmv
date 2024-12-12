62549694a1787df5a8fdb41166e01aedee6ac50d
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 14:40:52 -0600

    Fix typo in test name

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Registrant do
         end
 
 
-        it "has license date" do
+        it "has license data" do
             expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
             expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         end
