27a5fba5c750a95e1a9d0528ad40ad75150feb3c
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 13:35:26 -0600

    Add permit? method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -4,10 +4,15 @@ class Registrant
     def initialize(name, age, permit = false)
         @name = name
         @age = age
+        @permit = permit
         @license_data = {
             :written=>false,
             :license=>false,
             :renewed=>false
         }
+    end
+
+    def permit?
+        return @permit
     end
 end
