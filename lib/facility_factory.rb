baf53ccbaba984f2ad9df4e9cdb0ebb17f224996
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 21:36:17 -0600

    Add create_facilities method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,48 @@
 class FacilityFactory
-    
+    def create_facilities(dataset)
+        facilities = []
+
+        dataset.each do |facility|
+            case dataset[0][:state]
+            when "CO"
+                facility_address = facility[:address_li]
+                facility_address << " #{facility[:address__1]}" if facility.include?(:address__1)
+
+                facility_name = facility[:dmv_office]
+
+                facility_phone = facility[:phone]
+
+                services = facility[:services_p].split(/[,;]/)
+                services = services.map {|e| e.strip.split.map(&:capitalize).join(" ")}
+            when "NY"
+                facility_address = facility[:street_address_line_1]
+                facility_address << " #{facility[:street_address_line_2]}" if facility.include?(:street_address_line_2)
+
+                facility_name = "#{facility[:office_name]} #{facility[:office_type]}"
+
+                facility_phone = facility[:public_phone_number]
+
+                services = []
+            when "MO"
+                facility_address = facility[:address1]
+
+                facility_name = facility[:name]
+
+                facility_phone = facility[:phone]
+
+                services = []
+            end
+
+            new_facility = Facility.new({
+                :name=>facility_name,
+                :address=>facility_address,
+                :phone=>facility_phone
+            })
+            
+            services.each {|service| new_facility.add_service(service)}
+            facilities << new_facility
+        end
+
+        facilities
+    end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,4 +1,7 @@
 require './lib/facility_factory'
+require './lib/dmv_data_service'
+require './lib/facility'
+require 'pry'
 
 RSpec.describe FacilityFactory do
     before(:each) do
@@ -8,6 +11,38 @@ RSpec.describe FacilityFactory do
     describe "initialize" do
         it "exists" do
             expect(@factory).to be_a(FacilityFactory)
+        end
+    end
+
+    describe "create_facilities" do
+        it "can create facilities with the co dataset" do
+            facilities = @factory.create_facilities(DmvDataService.new.co_dmv_office_locations)
+            
+            expect(facilities[0]).to be_a(Facility)
+            expect(facilities[0].name).to eq("DMV Tremont Branch")
+            expect(facilities[0].address).to eq("2855 Tremont Place Suite 118")
+            expect(facilities[0].phone).to eq("(720) 865-4600")
+            expect(facilities[0].services).to eq(["Vehicle Titles", "Registration", "Renewals", "Vin Inspections"])
+        end
+
+        it "can create facilities with the ny dataset" do
+            facilities = @factory.create_facilities(DmvDataService.new.ny_dmv_office_locations)
+            
+            expect(facilities[0]).to be_a(Facility)
+            expect(facilities[0].name).to eq("LAKE PLACID COUNTY OFFICE")
+            expect(facilities[0].address).to eq("2693 MAIN STREET")
+            expect(facilities[0].phone).to eq(nil)
+            expect(facilities[0].services).to eq([])
+        end
+
+        it "can create facilities with the mo dataset" do
+            facilities = @factory.create_facilities(DmvDataService.new.mo_dmv_office_locations)
+            
+            expect(facilities[0]).to be_a(Facility)
+            expect(facilities[0].name).to eq("Harrisonville")
+            expect(facilities[0].address).to eq("2009 Plaza Dr.")
+            expect(facilities[0].phone).to eq("(816) 884-4133")
+            expect(facilities[0].services).to eq([])
         end
     end
 end
