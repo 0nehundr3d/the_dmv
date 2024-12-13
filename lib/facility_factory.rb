9e1f004a5199cee17c1f391a7c948c9d0eb1c471
Sebastian McKee <sebmcke@gmail.com>
2024-12-12 20:24:37 -0600

    Create FacilityFactory class

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,1 +1,3 @@
 class FacilityFactory
+    
+end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,5 +1,13 @@
 require './lib/facility_factory'
 
 RSpec.describe FacilityFactory do
-    
+    before(:each) do
+        @factory = FacilityFactory.new
+    end
+
+    describe "initialize" do
+        it "exists" do
+            expect(@factory).to be_a(FacilityFactory)
+        end
+    end
 end
